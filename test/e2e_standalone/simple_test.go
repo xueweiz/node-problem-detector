d346b36b66ad4c28db781a517996c3d4052b8055
Xuewei Zhang <xueweiz@google.com>
2019-06-21 15:00:55 -0700

    test

--- test/e2e_standalone/simple_test.go
+++ test/e2e_standalone/simple_test.go
@@ -1,11 +1,33 @@
-package books_test
+package e2e_standalone_test
 
 import (
-    . "github.com/onsi/ginkgo"
-    "testing"
+	"os"
+	"path"
+	"testing"
+
+	"k8s.io/kubernetes/test/e2e/framework/ssh"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/reporters"
 )
 
-func TestBooks(t *testing.T) {
-    RegisterFailHandler(Fail)
-    RunSpecs(t, "Books Suite")
+func TestNPD(t *testing.T) {
+	junitFile := "junit.xml"
+	artifacts_dir := os.Getenv("ARTIFACTS")
+	if artifacts_dir != "" {
+		junitFile = path.Join(artifacts_dir, junitFile)
+	}
+
+	junitReporter := reporters.NewJUnitReporter(junitFile)
+	ginkgo.RunSpecsWithDefaultAndCustomReporters(t, "NPD Standalone Suite", []ginkgo.Reporter{junitReporter})
 }
+
+var _ = ginkgo.Describe("NPD", func() {
+	ginkgo.It("dummy test foo", func() {
+		ssh.SSH("pwd", "34.83.159.130", "gce")
+		return
+	})
+	ginkgo.It("dummy test bar", func() {
+		return
+	})
+})
